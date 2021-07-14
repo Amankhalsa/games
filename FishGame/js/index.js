6aaf42945e538a82250c16c52707399728fe3277
Amankhalsa <66160544+Amankhalsa@users.noreply.github.com>
2021-07-14 19:44:33 +0530

    Update index.js

--- FishGame/js/index.js
+++ FishGame/js/index.js
@@ -1,7 +1,7 @@
 // Game Constants & Variables
 let inputDir = {x: 0, y: 0}; 
 const foodSound = new Audio('music/food.mp3');
-const gameOverSound = new Audio('music/over.wav');
+const gameOverSound = new Audio('music/over.mp3');
 const moveSound = new Audio('music/move.mp3');
 const musicSound = new Audio('music/music2.mp3');
 let speed = 8;
